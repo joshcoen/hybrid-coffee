7b25231be2f2daa11ee07fda4c26aa8d01c28b08
joshcoen <josh.coen@gmail.com>
2021-11-16 22:48:20 -0500

    testing temp sensors

--- pyscripts/temp_test.py
+++ pyscripts/temp_test.py
@@ -1,5 +1,6 @@
 import glob
 import time
+import math
 from influxdb import InfluxDBClient
 client = InfluxDBClient(host='192.168.100.157')
 
@@ -7,9 +8,9 @@ sensor1 = '/sys/bus/w1/devices/28-01204fe74e92/w1_slave'
 sensor2 = '/sys/bus/w1/devices/28-01204fee0355/w1_slave'
 sensor3 = '/sys/bus/w1/devices/28-012050011f8c/w1_slave'
 sensor_ids = ['28-01204fe74e92', '28-01204fee0355', '28-012050011f8c']
-# print(sensor_ids[0])
-# print(sensor_ids[1])
-# print(sensor_ids[2])
+sensor = 4
+blue = 0
+
 
 def read_temp_raw():
     f = open(sensor1, 'r')
@@ -25,10 +26,10 @@ def read_temp_raw():
     f.close()
     return sensor1_lines, sensor2_lines, sensor3_lines
 
+
 def read_temp():
     sensordata_list = []
     lines = read_temp_raw()
-    # print(lines)
     senseid = 0
     for line in lines:
         current_sensor = sensor_ids[senseid]
@@ -43,11 +44,24 @@ def read_temp():
             sensordata_list.append((current_sensor, temp_f))
     return sensordata_list
 
+
+def get_ambient():
+    try:
+        sensortype = "ambientProbe"
+        sensorid = '76-014897f0912f4'
+        [temp,humidity] = grovepi.dht(sensor,blue)
+        if math.isnan(temp) == False and math.isnan(humidity) == False:
+            temp=temp*1.8+32
+            ambient = 'coffee_info,sensor_id=%s,sensor_type=%s temperature=%d,humidity=%d' % (sensorid, sensortype, temp, humidity)
+            client.write([ambient], {'db': 'hybrid-coffee'}, 204, 'line')
+    except IOError:
+        print ("Error")
+
+
 while True:
     sensorType = 'temperatureProbe'
     for value, key in read_temp():
-        # print(value, key)
-
-        line = 'coffee_info,sensorId=%s,sensorType=%s temperature=%s' % (value, sensorType, key)
-        client.write([line], {'db': 'hybrid-coffee'}, 204, 'line')
+        temp = 'coffee_info,sensor_id=%s,sensor_type=%s temperature=%s' % (value, sensorType, key)
+        client.write([temp], {'db': 'hybrid-coffee'}, 204, 'line')
         time.sleep(1)
+    get_ambient()
