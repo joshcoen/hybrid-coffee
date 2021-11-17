d690b2b3981c977c89917271dc916aae88ca6b17
joshcoen <josh.coen@gmail.com>
2021-11-16 20:58:01 -0500

    testing temp sensors

--- pyscripts/temp_test.py
+++ pyscripts/temp_test.py
@@ -7,21 +7,30 @@ sensor3 = '/sys/bus/w1/devices/28-012050011f8c/w1_slave'
 
 def read_temp_raw():
     f = open(sensor1, 'r')
-    lines = f.readlines()
+    sensor1_lines = f.readlines()
     f.close()
-    return lines
+
+    f = open(sensor2, 'r')
+    sensor2_lines = f.readlines()
+    f.close()
+
+    f = open(sensor3, 'r')
+    sensor3_lines = f.readlines()
+    f.close()
+    return sensor1_lines, sensor2_lines, sensor3_lines
 
 def read_temp():
     lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+    for line in lines:
+        while line[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            sensor_lines = read_temp_raw()
+        equals_pos = line[1].find('t=')
+        if equals_pos != -1:
+            temp_string = line[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            return temp_f
 
 while True:
     print(read_temp())
