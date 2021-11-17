f38149e571b2a3bedac504387ebd02a7f05c5994
joshcoen <josh.coen@gmail.com>
2021-11-16 21:33:37 -0500

    testing temp sensors

--- pyscripts/temp_test.py
+++ pyscripts/temp_test.py
@@ -6,7 +6,9 @@ sensor1 = '/sys/bus/w1/devices/28-01204fe74e92/w1_slave'
 sensor2 = '/sys/bus/w1/devices/28-01204fee0355/w1_slave'
 sensor3 = '/sys/bus/w1/devices/28-012050011f8c/w1_slave'
 sensor_ids = ['28-01204fe74e92', '28-01204fee0355', '28-012050011f8c']
-print(sensor_ids)
+print(sensor_ids[0])
+print(sensor_ids[1])
+print(sensor_ids[2])
 
 def read_temp_raw():
     f = open(sensor1, 'r')
@@ -28,7 +30,8 @@ def read_temp():
         senseid = 0
         sensor = sensor_ids[senseid]
         print(sensor)
-        senseid+=1
+        senseid += 1
+        print(senseid)
         equals_pos = line[1].find('t=')
         if equals_pos != -1:
             temp_string = line[1][equals_pos+2:]
