1adda55ee8e9d4834e41cbbc6b8020d5d6964a2e
joshcoen <josh.coen@gmail.com>
2021-11-16 21:21:50 -0500

    testing temp sensors

--- pyscripts/temp_test.py
+++ pyscripts/temp_test.py
@@ -29,13 +29,13 @@ def read_temp():
         senseid+=1
         while line[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            sensor_lines = read_temp_raw()
+            line = read_temp_raw()
         equals_pos = line[1].find('t=')
         if equals_pos != -1:
             temp_string = line[1][equals_pos+2:]
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
-            return sensor, temp_f
+        return sensor, temp_f
 
 while True:
     print(read_temp())
