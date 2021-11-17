088c86edafe126ab54f6fe6aa6334f902c77dc88
joshcoen <josh.coen@gmail.com>
2021-11-16 22:13:15 -0500

    testing temp sensors

--- pyscripts/temp_test.py
+++ pyscripts/temp_test.py
@@ -41,8 +41,7 @@ def read_temp():
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
             sensordata += current_sensor, temp_f
-    for
-    print(sensordata)
+    # print(sensordata)
     return sensordata
 
 while True:
