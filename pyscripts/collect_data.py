c7f3d01b28fc5f8a1b8dd46eb78c7567e8c6749f
joshcoen <josh.coen@gmail.com>
2021-11-22 10:03:39 -0500

    adding get_weight code to main collect_data

--- pyscripts/collect_data.py
+++ pyscripts/collect_data.py
@@ -2,6 +2,7 @@ import glob
 import grovepi
 import time
 import math
+import sys
 from influxdb import InfluxDBClient
 client = InfluxDBClient(host='192.168.100.154')
 
@@ -45,11 +46,47 @@ def get_ambient():
     except IOError:
         print ("Error")
 
-#def get_weight():
+def get_weight():
+    EMULATE_HX711 = False
+
+    referenceUnit = 479.5
+
+    if not EMULATE_HX711:
+        import RPi.GPIO as GPIO
+        from hx711py.hx711 import HX711
+    else:
+        from hx711py.emulated_hx711 import HX711
+
+    def cleanAndExit():
+        print("Cleaning...")
+
+        if not EMULATE_HX711:
+            GPIO.cleanup()
+
+        print("Bye!")
+        sys.exit()
+
+    hx = HX711(23, 24)
+    hx.set_reading_format("MSB", "MSB")
+    hx.set_reference_unit(referenceUnit)
+    hx.reset()
+    hx.tare()
+
+    # print("Tare done! Add weight now...")
+
+    while True:
+        try:
+            val = max(0, int(hx.get_weight(5)))
+            print(val)
+            hx.power_down()
+            hx.power_up()
+            time.sleep(0.1)
+        except (KeyboardInterrupt, SystemExit):
+            cleanAndExit()
 
 
 while True:
-    read_temp()
-    get_ambient()
-    #get_weight()
+    #read_temp()
+    #get_ambient()
+    get_weight()
     time.sleep(1)
