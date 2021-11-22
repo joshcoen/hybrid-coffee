5fe6d7d91ccb4c79c0702e1b88f03e358073c1b0
joshcoen <josh.coen@gmail.com>
2021-11-22 09:25:12 -0500

    matching vistry schema

--- pyscripts/collect_data.py
+++ pyscripts/collect_data.py
@@ -27,25 +27,26 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         # return temp_sensor_id, temp_f
-        temp = 'coffee_info,sensor_id=%s,sensor_type=%s temperature=%s' % (temp_sensor_id, sensor_type, temp_f)
-        print(temp)
-        # client.write([temp], {'db': 'hybrid-coffee'}, 204, 'line')
+        temp = 'coffee_info,location=3445,sensor_id=%s,sensor_type=%s sensor_value=%s' % (temp_sensor_id, sensor_type, temp_f)
+        # print(temp)
+        client.write([temp], {'db': 'hybrid-coffee'}, 204, 'line')
 
 
 def get_ambient():
     try:
-        sensortype = "ambientProbe"
-        sensorid = '76-014897f0912f'
+        sensor_id = '76-014897f0912f'
         [temp,humidity] = grovepi.dht(sensor,blue)
         if math.isnan(temp) == False and math.isnan(humidity) == False:
             temp=temp*1.8+32
-            ambient = 'coffee_info,sensor_id=%s,sensor_type=%s temperature=%d,humidity=%d' % (sensorid, sensortype, temp, humidity)
-            client.write([ambient], {'db': 'hybrid-coffee'}, 204, 'line')
+            ambient_temp = 'coffee_info,sensor_id=%s,sensor_type="ambinet_temperature", sensor_value=%d' % (sensor_id, temp)
+            ambient_humidity =  'coffee_info,sensor_id=%s,sensor_type=humidity, sensor_value=%d' % (sensor_id, humidity)
+            client.write([ambient_temp], {'db': 'hybrid-coffee'}, 204, 'line')
+            client.write([ambient_humidity], {'db': 'hybrid-coffee'}, 204, 'line')
     except IOError:
         print ("Error")
 
 
 while True:
     read_temp()
-    # get_ambient()
+    get_ambient()
     time.sleep(1)
