0441815766eabbb1f51ac147a1f7f06d1e5f307f
joshcoen <josh.coen@gmail.com>
2021-11-22 12:13:22 -0500

    testing new structure

--- pyscripts/temp_test.py
+++ pyscripts/temp_test.py
@@ -1,46 +1,93 @@
-#!/usr/bin/python3
-from hx711 import HX711  # import the class HX711
-import RPi.GPIO as GPIO  # import GPIO
+import glob
+import grovepi
 import time
+import math
+import sys
+from influxdb import InfluxDBClient
+client = InfluxDBClient(host='192.168.100.154')
 
-try:
-    ########## Change the pin values below to the pins you are using ###################
-    DataPin = 5
-    ClockPin = 6
-    NumReadings = 10
+temp_sensor1 = '/sys/bus/w1/devices/28-01204fe74e92/w1_slave'
+temp_sensor_id = '28-01204fe74e92'
+sensor = 4
+blue = 0
 
-    print("Reading HX711")
-    # Create an object hx which represents your real hx711 chip
-    # Required input parameters are only 'dout_pin' (data) and 'pd_sck_pin' (clock)
-    # If you do not pass any argument 'gain_channel_A' then the default value is 128
-    # If you do not pass any argument 'set_channel' then the default value is 'A'
-    # you can set a gain for channel A even though you want to currently select channel B
-    hx = HX711(dout_pin=DataPin, pd_sck_pin=ClockPin, gain=128, channel='A')
+def get_data():
 
-    print("Reset")
-    result = hx.reset()  # Before we start, reset the hx711 ( not necessary)
-    if result:  # you can check if the reset was successful
-        print('Ready to use')
+    def read_temp_raw():
+        f = open(temp_sensor1, 'r')
+        sensor1_lines = f.readlines()
+        f.close()
+        return sensor1_lines
+
+
+    def read_temp():
+        sensor_type = 'temperature'
+        lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            # return temp_sensor_id, temp_f
+            temp = 'coffee_info,location_id=3445,sensor_type=%s,sensor_id=%s sensor_value=%s' % (sensor_type, temp_sensor_id, temp_f)
+            # print(temp)
+            client.write([temp], {'db': 'hybrid-coffee'}, 204, 'line')
+
+
+    def get_ambient():
+        try:
+            sensor_id = '76-014897f0912f'
+            [temp,humidity] = grovepi.dht(sensor,blue)
+            if math.isnan(temp) == False and math.isnan(humidity) == False:
+                temp=temp*1.8+32
+                ambient_temp = 'coffee_info,location_id=3445,sensor_type=ambient_temperature,sensor_id=%s sensor_value=%d' % (sensor_id, temp)
+                ambient_humidity = 'coffee_info,location_id=3445,sensor_type=humidity,sensor_id=%s sensor_value=%d' % (sensor_id, humidity)
+                client.write([ambient_temp], {'db': 'hybrid-coffee'}, 204, 'line')
+                client.write([ambient_humidity], {'db': 'hybrid-coffee'}, 204, 'line')
+        except IOError:
+            print ("Error")
+
+    EMULATE_HX711 = False
+
+    referenceUnit = 479.5
+
+    if not EMULATE_HX711:
+        import RPi.GPIO as GPIO
+        from hx711py.hx711 import HX711
     else:
-        print('not ready')
+        from hx711py.emulated_hx711 import HX711
 
-    # Read data several, or only one, time and return mean value
-    # it just returns exactly the number which hx711 sends
-    # argument times is not required default value is 1
-    while NumReadings is 10:
-        data = hx.get_raw_data(NumReadings)
-        subtotal = 0
-        for dt in data:
-            subtotal += dt
-        total = subtotal / 10
-        if data != False:  # always check if you get correct value or only False
-            # print('Raw data: ' + str(data))
-            print(total)
-        else:
-            print('invalid data')
+    hx = HX711(23, 24)
+    hx.set_reading_format("MSB", "MSB")
+    hx.set_reference_unit(referenceUnit)
+    hx.reset()
+    hx.tare()
+    # print("Tare done! Add weight now...")
 
-except (KeyboardInterrupt, SystemExit):
-    print('Bye :)')
+    def cleanAndExit():
+        print("Cleaning...")
 
-finally:
-    GPIO.cleanup()
+        if not EMULATE_HX711:
+            GPIO.cleanup()
+
+        print("Bye!")
+        sys.exit()
+
+    while true:
+        #read_temp()
+        #get_ambient()
+        # time.sleep(1)
+        try:
+            sensor_type = 'weight'
+            weight_sensor_id = '84-0760223ee18d'
+            weight_val = max(0, int(hx.get_weight(5)))
+            weight = 'coffee_info,location_id=3445,sensor_type=%s,sensor_id=%s sensor_value=%s' % (sensor_type, weight_sensor_id, weight_val)
+            # client.write([weight], {'db': 'hybrid-coffee'}, 204, 'line')
+            print(weight)
+            hx.power_down()
+            hx.power_up()
+        except (KeyboardInterrupt, SystemExit):
+            cleanAndExit()
+
+
+get_data()
