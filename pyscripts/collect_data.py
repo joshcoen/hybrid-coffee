6215d912f51775599c09f8e2ce0038fecc71fa78
joshcoen <josh.coen@gmail.com>
2021-11-22 10:30:37 -0500

    updating def_weight to write to influx

--- pyscripts/collect_data.py
+++ pyscripts/collect_data.py
@@ -68,7 +68,7 @@ def set_tare():
 
 def get_weight():
     EMULATE_HX711 = False
-    referenceUnit = 0
+    referenceUnit = 1
     if not EMULATE_HX711:
         import RPi.GPIO as GPIO
         from hx711py.hx711 import HX711
